c3e70d3a912362225a55a22d10bdf38b1b6e31a6
Dmitry Mosquid <mosquid@gmail.com>
2023-05-12 20:29:16 +0300

    env vars added

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    API_KEY: process.env.API_KEY,
+    REDIS_ENDPOINT: process.env.REDIS_ENDPOINT,
+    REDIS_PASSWORD: process.env.REDIS_PASSWORD,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
