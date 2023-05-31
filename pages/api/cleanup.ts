53e669f8243921959786b490ff993e2321031870
Dmitry Mosquid <mosquid@gmail.com>
2023-05-31 19:12:23 +0300

    migrated to gpt-overlord

--- pages/api/cleanup.ts
+++ pages/api/cleanup.ts
@@ -1,49 +1,42 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { set } from "@/services/redis";
 import type { NextApiRequest, NextApiResponse } from "next";
-import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from "openai";
+import GPTOverlord from "gpt-overlord";
 
-const API_KEY = process.env.API_KEY;
+const API_KEY: string = process.env.API_KEY || "";
 
-const configuration = new Configuration({
+const overlord = new GPTOverlord({
   apiKey: API_KEY,
+  model: "gpt-3.5-turbo",
+  temperature: 0.1,
 });
-const openai = new OpenAIApi(configuration);
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   const { text } = req.body;
-  const messages: Array<ChatCompletionRequestMessage> = [
-    {
-      content: `Hey. I have a ukrainian text which contains surzhuk and russian words and perhaps incorrect grammar. I need to translate it to ukrainian. Could you help me? Here is the text: 
+  const prompt = `I have a ukrainian text which contains surzhuk and russian words and perhaps incorrect grammar. I need you to proofread it fix any mistakes. Here is the text: 
       ---
       ${text}. 
       ---
-      ###
-      The message you send back to me should not contain any additional information and should only provide a translated version of the input text.`,
-      role: "user",
-    },
-  ];
+      You should strictly follow the specified schema. Any additional text outside the schema will cause errors on the receiving node.
+      The entire answer must be a single JSON object. Any additional text should be ommited. No comments allowed`;
 
   const ts = new Date().getTime();
   try {
     res.status(200).json({ ts });
+    const response = await overlord.prompt(prompt);
 
-    const response = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      temperature: 0.1,
-      messages,
-    });
-    const { message } = response.data.choices[0];
-    const { content = "" } = message || {};
+    const { data: content, status }: { status: string; data: string } =
+      response;
 
-    console.log(response.data.choices);
+    if (status !== "success") {
+      throw new Error("GPT3 error" + content);
+    }
 
     await set(ts.toString(), { content });
   } catch (error) {
-    console.error(error);
     await set(ts.toString(), {
       error: (error as Error).message,
       content: "",
